b2ca366136fc7130ebc32143f5299844fa843527
EderlMarkus <office@edma.at>
2019-06-06 12:07:38 +0200

    updated PostHandler Class and cleaned up index.php

--- PostHandler.php
+++ PostHandler.php
@@ -2,6 +2,19 @@
 
 class FormHandler
 {
+
+    private $connection;
+
+    /**
+     * Constructor: Requires host-, database- & username as well as password of the SQL-database you want to write in.
+     */
+    function __construct($host, $user,  $db, $pass) {
+        $this->connection = new mysqli($host, $user, $pass, $db) or die('Unable to connect');
+    }
+
+    /**
+     * Testing input to make sure no harmful code can be pushed. Requries a String as Parameter and returns the String after checking.
+     */
     public function test_input($data)
     {
         $data = trim($data);
@@ -10,29 +23,33 @@ class FormHandler
         return $data;
     }
 
-    public function addLineToTableInDatabase($table, $user, $data, $databaseconnection)
+    /**
+     * Adds niew Line to a Table in the Database. Returns boolean if successfull or not. Currently writes hardcoded
+     * in the columns "user" and "data". 
+     */
+    public function addLineToTableInDatabase($table, $user, $data)
     {
         $sql = "INSERT INTO " . $table . " (User, data) VALUES ('" . $user . "','" . $data . "') ";
-        return $databaseconnection->query($sql);
+        return $this->connection->query($sql);
     }
 
-    public function connectToMySQL($host, $user, $pass, $db)
-    {
-        $db = new mysqli($host, $user, $pass, $db) or die('Unable to connect');
-        return $db;
-    }
-
-    public function createNewSQLTable($tablename, $databaseconnection)
+    /**
+     * Creates a new Table in the SQL Database with the Predefined COlumns "User", "data" and "Time"
+     */
+    public function createNewSQLTable($tablename)
     {
         $sql = "CREATE TABLE " . $tablename . " (data varchar(255), User varchar(255), Time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP)";
-        $query = $databaseconnection->query($sql);
+        $query = $this->connection->query($sql);
         return $tablename;
     }
 
-    public function getTableAsArray($table, $databaseconnection)
+    /**
+     * Converts the given SQL-Table to an Array and returns the array.
+     */
+    public function getTableAsArray($table)
     {
         $sql = "SELECT * FROM " . $table;
-        $result = $databaseconnection->query($sql);
+        $result = $this->connection->query($sql);
         $returnarray = [];
         if ($result->num_rows > 0) {
             while ($row = mysqli_fetch_array($result)) {
@@ -42,9 +59,12 @@ class FormHandler
         return $returnarray;
     }
 
-    public function getTableAsHTMLTable($table, $databaseconnection)
+    /**
+     * Converts the given table to an HTML-Table and returns the table.
+     */
+    public function getTableAsHTMLTable($table)
     {
-        $tableAsArray = $this->getTableAsArray($table, $databaseconnection);
+        $tableAsArray = $this->getTableAsArray($table);
         $returnHTML = "<table id='table'><tbody><tr><th>Message</th><th>User</th><th>Timestamp</th></tr>";
 
         for ($i = 0; $i < sizeof($tableAsArray); $i++) {
--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
 session_start();
 require_once 'PostHandler.php';
-$FormHandler = new FormHandler;
-$connection = $FormHandler->connectToMySQL('localhost', 'root', '', 'testdb');
-$table = $FormHandler->createNewSQLTable("PHP_Database", $connection);
-
-//AutoFill of User-Field if User was already set
-function setUser($user)
-{
-    $FormHandler = new FormHandler;
-    $user = $FormHandler->test_input($user);
-    $_SESSION["User"] = $user;
-    echo "<script>document.getElementById('name').value = '" . $user . "'</script>";
-}
-
-if (isset($_SESSSION["User"])) {
-    setUser($user);
-}
+$FormHandler = new FormHandler('localhost', 'root', 'testdb','');
+$table = $FormHandler->createNewSQLTable("PHP_Database");
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -31,7 +16,7 @@ if (isset($_SESSSION["User"])) {
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form method="post">
+    <form method="post" action="">
         <p class="headers">Your Name:</p>
         <input id="name" name="User" type="text"></input>
         <p class="headers">Your Message:</p>
@@ -45,22 +30,49 @@ if (isset($_SESSSION["User"])) {
 
 <?php
 
+
+/**
+ * User-Managment: AutoFill of User-Field if User was already set */
+
+ /**
+  * setUser: Checks input, sets Session-Variable and fills out HTML Input Field with a JS.
+  */
+function setUser($user, $handler)
+{
+    echo "<script>console.log('hallo')</script>";
+    $user = $handler->test_input($user);
+    $_SESSION["User"] = $user;
+    echo "<script>document.getElementById('name').value = '" . $user . "'</script>";
+}
+
+/**
+ * Check if Session for Username is already set, if so call setuser
+ */
+if (isset($_SESSION["User"])) {
+    setUser($_SESSION["User"], $FormHandler);
+}
+
+/**
+ * Validation if the Input-Fields have been set, if so, call setuser, test input and add Line to Database.
+ * if not echo the missing Data.
+ */
+
 if (isset($_POST["message"]) && isset($_POST["User"])) {
     if (empty($_POST["User"]) && empty($_POST["message"])) {
         echo "Please enter your Name and your Message";
     } else if (empty($_POST["User"])) {
         echo "Please enter your Name";
     } else if (empty($_POST["message"])) {
-        setUser($_POST["User"]);
+        setUser($_POST["User"], $FormHandler);
         echo "Please enter your Message";
     } else {
-        setUser($_POST["User"]);
+        setUser($_POST["User"], $FormHandler);
         $testedMessage = $FormHandler->test_input($_POST['message']);
         $testedUser = $FormHandler->test_input($_POST['User']);
-        $FormHandler->addLineToTableInDatabase($table, $testedUser, $testedMessage, $connection);
-
-        echo $FormHandler->getTableAsHTMLTable($table, $connection);
+        $FormHandler->addLineToTableInDatabase($table, $testedUser, $testedMessage);
+        echo $FormHandler->getTableAsHTMLTable($table);
     }
+
 }
 
 ?>
